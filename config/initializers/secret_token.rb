4fa74ed7e364df7f76434209df9fb59324dd8ab7
Jason Draper <jason@drapergeek.com>
2013-06-03 14:12:56 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kix::Application.config.secret_key_base = 'b902114d8a2f2d702822f8b8b70d3c69a351bfa2d30055eca867436288644396d3b3be1d65b1db46eb53f3859c4959f1e651ce897abfeed20411fbbf76f8bf39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kix::Application.config.session_store :cookie_store, key: '_kix_session'
