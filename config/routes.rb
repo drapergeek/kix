0ed2e4cf0e0de2bae3389a2406e0cac8f9f49053
Jason Draper <jason@drapergeek.com>
2013-06-04 20:48:40 -0400

    Allow users to invite other users to a game.
    * Allow users to join games via link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Kix::Application.routes.draw do
   resources :games, only: [:new, :create, :show]
+  resources :invitations, only: [:new, :create, :show]
   root to: 'games#new'
 end
--- spec/features/game_setup_spec.rb
+++ spec/features/game_setup_spec.rb
@@ -11,6 +11,17 @@ feature 'game setup' do
     visit new_game_path
     verify_game_is_already_started
   end
+
+  scenario 'user sends invitations' do
+    setup_game
+    send_invitations
+    verify_invitations_were_sent
+  end
+
+  scenario 'user visits invitation link' do
+    click_invitation_link
+    verify_joined_game
+  end
 end
 
 def setup_game
@@ -20,11 +31,33 @@ end
 
 def verify_game_started
   expect(page).to have_content 'Your game has started'
-  expect(page).to have_selector('a', text: 'Setup your hero')
+  expect(page).to have_link 'Setup your hero'
 end
 
 def verify_game_is_already_started
   verify_game_started
   expect(page).to have_content 'You already have a game started.'
-
 end
+
+def send_invitations
+  click_link 'Invite Friends'
+  fill_in 'Emails', with: 'spiderman@example.com, wolverine@example.com'
+  click_button 'Invite'
+end
+
+def verify_invitations_were_sent
+  email = deliveries.last
+  expect(email.to).to include 'wolverine@example.com'
+  expect(email.to).to include 'spiderman@example.com'
+  expect(email.body).to include 'http://kix.dev/invitations'
+end
+
+ def click_invitation_link
+   game = Game.create
+   visit invitation_url(game)
+ end
+
+ def verify_joined_game
+   verify_game_started
+   expect(page).to have_content "Thanks for joining the game!"
+ end
