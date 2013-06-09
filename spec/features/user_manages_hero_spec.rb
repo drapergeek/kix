e83204c7a8a77705b137c90b5c6dc89b9653ec4a
Jason Draper <jason@drapergeek.com>
2013-06-09 09:58:27 -0400

    Clean up styles a bit to make the UI nicer.
    * Fix tests to respond appropriately to new UI.

--- spec/features/user_manages_hero_spec.rb
+++ spec/features/user_manages_hero_spec.rb
@@ -35,10 +35,9 @@ end
 
 def verify_character_information_is_shown
   expect(page).to have_content 'You have successfully created your character.'
-  expect(page).to have_content 'Player: Bob'
-  expect(page).to have_content 'Character: Legacy'
-  expect(page).to have_content 'Max HP: 25'
-  expect(page).to have_content 'Current HP: 25'
+  expect(page).to have_selector('h2', text: 'Legacy')
+  expect(page).to have_content 'Max: 25'
+  expect(page).to have_current_hp(25)
 end
 
 def lower_hp
@@ -50,13 +49,17 @@ def increase_hp
 end
 
 def verify_hp_is_lowered_by_one
-  expect(page).to have_content 'Current HP: 24'
+  expect(page).to have_current_hp(24)
 end
 
 def verify_hp_is_increased_by_one
-  expect(page).to have_content 'Current HP: 25'
+  expect(page).to have_current_hp(25)
 end
 
 def verify_hp_did_not_exceed_max
-  expect(page).to have_content 'Current HP: 25'
+  expect(page).to have_current_hp(25)
+end
+
+def have_current_hp(amount)
+  have_content "Current #{amount}"
 end
