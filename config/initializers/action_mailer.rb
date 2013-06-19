1d16441a3a6e8dfbcb6fe8c9dac6846d91224ee4
Jason Draper <jason@drapergeek.com>
2013-06-19 19:17:53 -0400

    Add mailgun config.

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,12 @@
+unless ['test', 'development'].include? Rails.env
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'kixit-staging.herokuapp.com',
+    :authentication => :plain,
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+end
